e0d547f7b8497e0b57d3e7dfd249c9b8481cc24e
Joakim Bengtson <joakim.bengtson@gmail.com>
2014-03-28 13:40:55 +0100

    Small refactorings

--- index.js
+++ index.js
@@ -5,21 +5,23 @@ var estraverse  = require('estraverse');
 module.exports = function angularFilesort () {
   var files = [];
   var angmods = {};
+  var indexCache = {};
+
   return es.through(function (file) {
-      // console.log(file.path);
       var declared = [],
           used = [];
+
       estraverse.traverse(esprima.parse(file.contents), {
         leave: function (node, parent) {
-          if (node.type !== 'MemberExpression' || node.object.name !== 'angular' || node.property.name !== 'module') {
+          if (!isAngularModuleStatement(node)) {
             return;
           }
-          var name = parent.arguments[0].value;
+          var moduleName = parent.arguments[0].value;
           if (parent.arguments[1]) {
-            if (declared.indexOf(name) < 0) {
-              declared.push(name);
-              if (used.indexOf(name) > -1) {
-                used.splice(used.indexOf(name), 1);
+            if (declared.indexOf(moduleName) < 0) {
+              declared.push(moduleName);
+              if (used.indexOf(moduleName) > -1) {
+                used.splice(used.indexOf(moduleName), 1);
               }
             }
             parent.arguments[1].elements.forEach(function (el) {
@@ -27,24 +29,24 @@ module.exports = function angularFilesort () {
                 used.push(el.value);
               }
             });
-          } else if (declared.indexOf(name) < 0 && used.indexOf(name) < 0) {
-            used.push(name);
+          } else if (declared.indexOf(moduleName) < 0 && used.indexOf(moduleName) < 0) {
+            used.push(moduleName);
           }
         }
       });
+
       for (var d = 0; d < declared.length; d++) {
-        if (angmods[declared[d]]) {
-          return this.emit('error', new Error('Module "' + declared[d] + '" in `' + file.relative + '`was already declared in `' + angmods[declared[d]].file.relative + '`'));
-        }
         angmods[declared[d]] = {
           file: file,
           name: declared[d],
           uses: used
         };
       }
+
       files.push({file: file, uses: used});
     }, function () {
-      var sorter = sortIndex.bind(null, angmods);
+      var sorter = sortIndex.bind(null, indexCache, angmods);
+
       files.sort(function (a, b) {
         var aIndex = sorter(a),
             bIndex = sorter(b);
@@ -62,10 +64,13 @@ module.exports = function angularFilesort () {
     });
 };
 
-var indexCache = {};
-function sortIndex (angmods, obj) {
-  if (typeof indexCache[obj.file.relative] !== 'undefined') {
-    return indexCache[obj.file.relative];
+function isAngularModuleStatement (node) {
+  return node.type === 'MemberExpression' && node.object.name === 'angular' && node.property.name === 'module';
+}
+
+function sortIndex (cache, angmods, obj) {
+  if (typeof cache[obj.file.relative] !== 'undefined') {
+    return cache[obj.file.relative];
   }
   var index = 0;
   if (obj.uses.length) {
@@ -74,11 +79,11 @@ function sortIndex (angmods, obj) {
       if (!mod) {
         continue;
       }
-      var modIndex = sortIndex(angmods, mod);
+      var modIndex = sortIndex(cache, angmods, mod);
       if (modIndex + 1 > index) {
         index = modIndex + 1;
       }
     }
   }
-  return indexCache[obj.file.relative] = index;
+  return cache[obj.file.relative] = index;
 }
